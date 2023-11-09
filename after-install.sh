29de0730f1f92311070d5ecccb8c641f0ed62f13
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-09 23:11:35 +0100

    fix after install ?

--- after-install.sh
+++ after-install.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 if [[ -d "/usr/local/share/openhd_platform/rock/rk3566" ]]; then
+    rm -Rf /etc/systemd/system/h26*
+    rm -Rf /etc/systemd/system/qopenhd*
     cp /etc/systemd/system/qopenhd/rock3_qopenhd.service /etc/systemd/system/qopenhd.service
     cp /etc/systemd/system/qopenhd/rock3_h264_decode.service /etc/systemd/system/h264_decode.service
     cp /etc/systemd/system/qopenhd/rock3_h265_decode.service /etc/systemd/system/h265_decode.service
 elif [[ -d "/usr/local/share/openhd_platform/rock/rock5a" ]]; then
+    rm -Rf /etc/systemd/system/h26*
+    rm -Rf /etc/systemd/system/qopenhd*
     cp /etc/systemd/system/qopenhd/rock5_qopenhd.service /etc/systemd/system/qopenhd.service
     cp /etc/systemd/system/qopenhd/rock5_h264_decode.service /etc/systemd/system/h264_decode.service
     cp /etc/systemd/system/qopenhd/rock5_h265_decode.service /etc/systemd/system/h265_decode.service
 elif [[ -d "/usr/local/share/openhd_platform/rock/rock5b" ]]; then
+    rm -Rf /etc/systemd/system/h26*
+    rm -Rf /etc/systemd/system/qopenhd* 
     cp /etc/systemd/system/qopenhd/rock5_qopenhd.service /etc/systemd/system/qopenhd.service
     cp /etc/systemd/system/qopenhd/rock5_h264_decode.service /etc/systemd/system/h264_decode.service
     cp /etc/systemd/system/qopenhd/rock5_h265_decode.service /etc/systemd/system/h265_decode.service
--- before-install.sh
+++ before-install.sh
@@ -2,5 +2,3 @@
 
 #remove custom QT
 dpkg -r openhd-qt-jammy || true
-rm -Rf /etc/systemd/system/h26*
-rm -Rf /etc/systemd/system/qopenhd*
