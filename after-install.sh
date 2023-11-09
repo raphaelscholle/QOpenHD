5729987d13b0e788023c1f702aa93aa1a1b81683
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-11-09 18:49:14 +0100

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if [[ -f "/usr/local/share/openhd_platform/rock/rk3566" ]]; then
-    /etc/systemd/system/qopenhd/rock3_qopenhd.service /etc/systemd/system/qopenhd.service
-    /etc/systemd/system/qopenhd/rock3_h264_decode.service /etc/systemd/system/h264_decode.service
-    /etc/systemd/system/qopenhd/rock3_h265_decode.service /etc/systemd/system/h265_decode.service
-else if [[ -f "/usr/local/share/openhd_platform/rock/rock5a" ]]; then
-    /etc/systemd/system/qopenhd/rock5_qopenhd.service /etc/systemd/system/qopenhd.service
-    /etc/systemd/system/qopenhd/rock5_h264_decode.service /etc/systemd/system/h264_decode.service
-    /etc/systemd/system/qopenhd/rock5_h265_decode.service /etc/systemd/system/h265_decode.service
-else if [[ -f "/usr/local/share/openhd_platform/rock/tock5b" ]]; then
-    /etc/systemd/system/qopenhd/rock5_qopenhd.service /etc/systemd/system/qopenhd.service
-    /etc/systemd/system/qopenhd/rock5_h264_decode.service /etc/systemd/system/h264_decode.service
-    /etc/systemd/system/qopenhd/rock5_h265_decode.service /etc/systemd/system/h265_decode.service
+    ln -s /etc/systemd/system/qopenhd/rock3_qopenhd.service /etc/systemd/system/qopenhd.service
+    ln -s /etc/systemd/system/qopenhd/rock3_h264_decode.service /etc/systemd/system/h264_decode.service
+    ln -s /etc/systemd/system/qopenhd/rock3_h265_decode.service /etc/systemd/system/h265_decode.service
+elif [[ -f "/usr/local/share/openhd_platform/rock/rock5a" ]]; then
+    ln -s /etc/systemd/system/qopenhd/rock5_qopenhd.service /etc/systemd/system/qopenhd.service
+    ln -s /etc/systemd/system/qopenhd/rock5_h264_decode.service /etc/systemd/system/h264_decode.service
+    ln -s /etc/systemd/system/qopenhd/rock5_h265_decode.service /etc/systemd/system/h265_decode.service
+elif [[ -f "/usr/local/share/openhd_platform/rock/tock5b" ]]; then
+    ln -s /etc/systemd/system/qopenhd/rock5_qopenhd.service /etc/systemd/system/qopenhd.service
+    ln -s /etc/systemd/system/qopenhd/rock5_h264_decode.service /etc/systemd/system/h264_decode.service
+    ln -s /etc/systemd/system/qopenhd/rock5_h265_decode.service /etc/systemd/system/h265_decode.service
 fi
 
 # the openhd scripts start the correct OSD depending on the distro and settings
@@ -20,10 +20,11 @@ if [ -f /etc/systemd/system/qopenhd.service ]; then
 else
     echo "QOpenHD is not started with a service on this system"
 fi
+
 #cd /usr/local/share/
 #if [ -d "/usr/local/share/testvideos" ]; then
 #  # Take action if $DIR exists. #
 #  echo "Testvideos are already there"
 #else
-#git clone https://github.com/OpenHD/testvideos/
+#git clone https://github.com/OpenHD/testvideos/ /usr/local/share/testvideos
 #fi
